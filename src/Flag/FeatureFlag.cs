efd6dd1b01ff5935b1c9a75b0f9bc715c7884a11
Jason Asare <jasonpowel2021@gmail.com>
2026-04-18 23:53:05 +0000

    add class library project

---
+++ src/Flag/FeatureFlag.cs
@@ -0,0 +1,9 @@
+﻿namespace Flag;
+
+public record struct FeatureFlag(string Name, bool IsEnabled)
+{
+	public static FeatureFlag Create(string name, bool isEnabled)
+	{
+		return new FeatureFlag(name, isEnabled);
+	}
+};
